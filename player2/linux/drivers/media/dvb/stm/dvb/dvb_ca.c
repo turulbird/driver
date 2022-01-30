c00e50731b8812154d8481a87de0cec2782b8b45
Audioniek <audioniek@xs4all.nl>
2022-01-30 22:13:23 +0100

    Enable build for Power Vu DES

--- player2/linux/drivers/media/dvb/stm/dvb/dvb_ca.c
+++ player2/linux/drivers/media/dvb/stm/dvb/dvb_ca.c
@@ -217,6 +217,7 @@ static int CaIoctl(struct inode *Inode, struct file *File, unsigned int IoctlCod
 			return 0;
 			break;
 		}
+#if defined(POWER_VU_DES)
 		case CA_SET_DESCR_MODE:
 		{
 			ca_descr_mode_t *descr_mode = (ca_descr_mode_t*) Parameter;
@@ -258,6 +259,7 @@ static int CaIoctl(struct inode *Inode, struct file *File, unsigned int IoctlCod
 			return 0;
 			break;
 		}
+#endif
 		case CA_SET_DESCR:
 		{
 			ca_descr_t *descr = (ca_descr_t *) Parameter;
